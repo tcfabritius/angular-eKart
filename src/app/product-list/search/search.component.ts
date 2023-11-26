3153783eddb70c0f36d7a83a9f557c9a8a31c4bd
Tim Fabritius <tim.fabritius@skiff.com>
2023-11-26 11:33:21 +0200

    search

---
+++ src/app/product-list/search/search.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  searchText:string="Mens Wear";
+
+  updateSearchText(event: any){
+    this.searchText = event.target.value;
+  }
+}
