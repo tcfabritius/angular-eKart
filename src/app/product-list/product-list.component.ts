76808663a40ddf7cdd0c2e230bddd02a674d4dbb
Tim Fabritius <tim.fabritius@skiff.com>
2023-11-27 13:17:14 +0200

    main menu

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,36 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  //name = "John Doe";
-  addToCart:number=0;
-  product = {
-    name: 'iPhone X',
-    price: 799,
-    color: 'red',
-    discount: 8.5,
-    inStock: 10,
-    pImage:'/assets/kuva.jpg'
-  }
-
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount / 100);
-  }
-
-  onNameChange(event: any){
-    //this.name = event.target.value;
-  }
-
-  decrementCartValue(){
-    if(this.addToCart>0){
-      this.addToCart--;
-    }
-    
-  }
-  
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-    
-  }
+  listOfString:string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah']
 }
